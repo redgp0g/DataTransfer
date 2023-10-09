cb872214f3da9b5afc77f6647e81d10408e3bc44
Guilherme Gordiano <guilhermeg2004@gmail.com>
2023-10-09 08:01:35 -0300

    Melhorias - Adicionado Logging - Adicionado função Buscar linha Cota - Dinamica para buscar código padrão no zeiss - Correção de 'codigo != ""' para None - Buscar o código na linha da peça ao invés da máquina - Transforma valores em Decimal ao atribuir para array (MEA)

--- IVR.py
+++ IVR.py
@@ -1,3 +1,4 @@
+import logging
 from contextlib import nullcontext
 from datetime import datetime
 from decimal import Decimal
@@ -7,13 +8,30 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import xlwings as xw
 
-pastas_planilhas = [r'\\Sch-fns03a\ds1\Inovacao1\Guilherme\Projetos\Novo IVR\Teste']
+log_directory = r'\\Sch-fns03a\ds1\Producao2\Registro de Inspeção\Registros PDF'
 
-pastas_monitoradas = [r'\\Sch-fns03a\ds1\Inovacao1\Guilherme\Projetos\Novo IVR\Teste']
+log_filename = 'log_relatorio_dimensional.log'
 
+log_path = os.path.join(log_directory, log_filename)
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+pastas_planilhas = [r'\\Sch-fns03a\ds1\Producao2\Registro de Inspeção\Spacer',
+                    r'\\Sch-fns03a\ds1\Producao2\Registro de Inspeção\Taurus',
+                    r'\\Sch-fns03a\ds1\Producao2\Registro de Inspeção\Bosch',
+                    r'\\Sch-fns03a\ds1\Producao2\Registro de Inspeção\Schaeffler']
+
+pastas_monitoradas = [r'\\Sch-fns03a\ds1\Qualidade1\2021\05.Metrologia\2021\23 Cep Zeiss',
+                        r'\\Sch-fns03a\ds1\Qualidade1\2021\05.Metrologia\2021\23 Cep Dea',]
+
+def buscar_primeira_linha_cota(linhas):
+    for i, linha in enumerate(linhas):
+        if "Cota" in linha or "COTA" in linha:
+            return linhas[i:]
+    return None
+                        
 def encontrar_caminho_planilha(codigo):
     caminho_planilha = None
-
     for pasta in pastas_planilhas:
         for arquivo in os.listdir(pasta):
             if arquivo.endswith('.xlsm'):
@@ -22,13 +40,14 @@ def encontrar_caminho_planilha(codigo):
     return caminho_planilha
 
 def buscar_dados_zeiss(todas_linhas,palavras_chave):
+    padrao = "M" + str(datetime.now().year)[2:]
     dados_medicao = None
     codigo = None
     for i, linha in enumerate(todas_linhas):
         for palavra in palavras_chave:
             if palavra in linha:
                 dados_medicao = todas_linhas[i + 1].strip()
-                codigo = dados_medicao[55:66].strip()
+                codigo = dados_medicao[dados_medicao.index(padrao):][:10].strip()
                 return codigo,dados_medicao
 
 def ler_arquivo_txt_zeiss(caminho):
@@ -38,10 +57,10 @@ def ler_arquivo_txt_zeiss(caminho):
             
             codigo,dados_medicao = buscar_dados_zeiss(todas_linhas,["Plano Medição","ID Teste","Data","Comentario"])
 
-            if(codigo != ""):
+            if(codigo != None):
                 caminho_planilha = encontrar_caminho_planilha(codigo)
                 if(caminho_planilha == None):
-                    print("Planilha não encontrada")
+                    logging.warning("Planilha não encontrada!")
                 else:
                     nomes_cotas = []
                     valores_encontrados = []
@@ -50,18 +69,15 @@ def ler_arquivo_txt_zeiss(caminho):
                     tolerancias_inferiores = []
                     desvios = []
 
-                    for i, linha in enumerate(todas_linhas):
-                        if "Cota" in linha or "COTA" in linha:
-                            cotas_linhas = todas_linhas[i:]
-                            break
+                    cotas_linhas = buscar_primeira_linha_cota(todas_linhas)
 
-                    for info_cotas in cotas_linhas:
-                        if info_cotas and not info_cotas[:25].isspace(): 
-                            valor_encontrado = info_cotas[35:46].strip()
-                            nominal = info_cotas[49:58].strip()
-                            tolerancia_superior = info_cotas[58:67].strip()
-                            tolerancia_inferior = info_cotas[67:76].strip()
-                            desvio = info_cotas[76:85].strip()
+                    for cota in cotas_linhas:
+                        if cota and not cota[:25].isspace(): 
+                            valor_encontrado = cota[35:46].strip()
+                            nominal = cota[49:58].strip()
+                            tolerancia_superior = cota[58:67].strip()
+                            tolerancia_inferior = cota[67:76].strip()
+                            desvio = cota[76:85].strip()
                             peca = dados_medicao[66:].strip()
 
                             if not tolerancia_superior:
@@ -74,7 +90,7 @@ def ler_arquivo_txt_zeiss(caminho):
                                 nominal  = "0.000"
                                 valor_encontrado = desvio
 
-                            nomes_cotas.append(info_cotas[:25].strip() + "_" + info_cotas[25:35].strip())
+                            nomes_cotas.append(cota[:25].strip() + "_" + cota[25:35].strip())
                             valores_encontrados.append(valor_encontrado)
                             nominais.append(nominal)
                             tolerancias_superiores.append(tolerancia_superior)
@@ -88,16 +104,17 @@ def ler_arquivo_txt_zeiss(caminho):
                     rng = planilha.range("A10:A698")
                     rng.api.Rows.Hidden = False
 
-                    primeira_celula_cota = nullcontext
+                    primeira_celula_cota = None
                     for linha_celula in range(10, 388 + 1):                    
                         if planilha.range("A" + str(linha_celula)).color == (0, 255, 0):
                             primeira_celula_cota = planilha.range("A" + str(linha_celula)).address
                             break
-                    if primeira_celula_cota == nullcontext:
+                    if primeira_celula_cota == None:
                         primeira_celula_cota = planilha.range("A10").end("down").offset(row_offset=1).address
                     linha_celula = int(primeira_celula_cota.split('$')[2])
 
                     celula_numero_peca = planilha.range("B699")
+
                     while celula_numero_peca.column < 26:  
                         if celula_numero_peca.value == float(peca):
                             break
@@ -144,11 +161,11 @@ def ler_arquivo_txt_zeiss(caminho):
                     workbook.save()
                     workbook.close()
                     app.quit()
-                    print("Dados transferidos corretamente!")
+                    logging.info("Dados transferidos com sucesso!")
             else:
-                print("Código não encontrado!")
+                logging.warning("Código não encontrado!")
     except Exception as e:
-        print(f"Ocorreu um erro em ler_arquivo_txt: {e}")
+        logging.error(f"Ocorreu um erro em ler_arquivo_txt: {e}")
     finally:
         try:
             workbook.close()
@@ -156,58 +173,53 @@ def ler_arquivo_txt_zeiss(caminho):
         except:
             pass
 
-def buscar_dados_mea_mistral(todas_linhas):
+def buscar_dados_mea(todas_linhas):
     padrao = "M" + str(datetime.now().year)[2:]
     codigo = None
     peca = None
     for i, linha in enumerate(todas_linhas):
-        if '02' in linha:
+        if '%52' in linha:
             if padrao in linha:
-                codigo = linha[linha.index(padrao):].strip()
-                peca = todas_linhas[i + 1][5:].strip()
+                index_padrao = linha.index(padrao)
+                codigo = linha[index_padrao:index_padrao + 9].strip()
+                peca = linha[index_padrao + 9:].strip()
                 break 
     return codigo, peca
 
-def ler_arquivo_mea_mistral(caminho):
+def ler_arquivo_mea(caminho):
     try:
         with open(caminho, 'r', encoding='latin-1') as arquivo:
             todas_linhas = arquivo.readlines()
             
-            codigo, peca = buscar_dados_mea_mistral(todas_linhas)       
+            codigo, peca = buscar_dados_mea(todas_linhas)       
                     
-            if codigo != "":
+            if codigo != None:
                 caminho_planilha = encontrar_caminho_planilha(codigo)
                 if not caminho_planilha:
-                    print("Planilha não encontrada")
-                else:
+                    logging.warning("Planilha não encontrada!")
+                else:         
+                    cotas_linhas = buscar_primeira_linha_cota(todas_linhas)           
+
                     nomes_cotas = []
                     valores_encontrados = []
                     nominais = []
                     tolerancias_superiores = []
                     tolerancias_inferiores = []
-                    
-                    for i, linha in enumerate(todas_linhas):
-                        if "Cota" in linha or "COTA" in linha:
-                            cotas_linhas = todas_linhas[i:]
-                            break
 
-                    for info_cotas in cotas_linhas:
-                            if info_cotas and not info_cotas[:25].isspace():
-                                if info_cotas[43:56].strip() != "0.000000" or info_cotas[56:68].strip() != "0.000000":
-                                    nomes_cotas.append(info_cotas[:14].strip() + "_" + info_cotas[14:16].strip())
-                                    valores_encontrados.append(info_cotas[16:31].strip())
-                                    nominais.append(info_cotas[30:44].strip())
-                                    tolerancias_superiores.append(info_cotas[56:68].strip())
-                                    tolerancias_inferiores.append(info_cotas[43:56].strip())
+                    for cota in cotas_linhas:
+                            if cota and not cota[:25].isspace():
+                                if not(Decimal(cota[56:68].strip()) == 0.000000 and Decimal(cota[56:68].strip()) == Decimal(cota[56:68].strip())):
+                                    nomes_cotas.append(cota[:14].strip() + "_" + cota[14:16].strip())
+                                    valores_encontrados.append(Decimal(cota[16:31].strip()))
+                                    nominais.append(Decimal(cota[30:44].strip()))
+                                    tolerancias_superiores.append(Decimal(cota[56:68].strip()))
+                                    tolerancias_inferiores.append(Decimal(cota[43:56].strip()))
 
                     app = xw.App(visible=False)
                     workbook = app.books.open(caminho_planilha)
                     planilha = workbook.sheets.active
-
                     planilha.api.Unprotect()
-
                     rng = planilha.range("A10:A698")
-
                     rng.api.Rows.Hidden = False
 
                     primeira_celula_cota = nullcontext
@@ -245,18 +257,18 @@ def ler_arquivo_mea_mistral(caminho):
                             planilha.range("A" + str(linha_celula)).color = (0, 255, 0)
 
                             if nominal == "0.000":
-                                planilha.range("Z" + str(linha_celula)).value = Decimal(nominal)
-                                planilha.range("AA" + str(linha_celula)).value = Decimal(nominal) + Decimal(tolsup)
-                                planilha.range("AD" + str(linha_celula)).value = Decimal(nominal)
-                                planilha.range("AE" + str(linha_celula)).value = Decimal(nominal) + Decimal(tolsup)
+                                planilha.range("Z" + str(linha_celula)).value = nominal
+                                planilha.range("AA" + str(linha_celula)).value = nominal + tolsup
+                                planilha.range("AD" + str(linha_celula)).value = nominal
+                                planilha.range("AE" + str(linha_celula)).value = nominal + tolsup
                             else:
-                                planilha.range("Z" + str(linha_celula)).value = Decimal(nominal) - Decimal(tolinf)
-                                planilha.range("AA" + str(linha_celula)).value = Decimal(nominal) + Decimal(tolsup)
+                                planilha.range("Z" + str(linha_celula)).value = nominal - tolinf
+                                planilha.range("AA" + str(linha_celula)).value = nominal + tolsup
 
-                                planilha.range("AD" + str(linha_celula)).value = Decimal(nominal) - (Decimal(tolinf) * Decimal(0.7))
-                                planilha.range("AE" + str(linha_celula)).value = Decimal(nominal) + (Decimal(tolsup) * Decimal(0.7))
+                                planilha.range("AD" + str(linha_celula)).value = nominal - (tolinf * Decimal(0.7))
+                                planilha.range("AE" + str(linha_celula)).value = nominal + (tolsup * Decimal(0.7))
 
-                            value_cell.value = valor_encontrado
+                            value_cell.value = str(valor_encontrado)
                                 
                             linha_celula = linha_celula + 1
 
@@ -270,10 +282,11 @@ def ler_arquivo_mea_mistral(caminho):
                     workbook.save()
                     workbook.close()
                     app.quit()
+                    logging.info("Dados transferidos com sucesso!")
             else:
-                print("Código não encontrado!")
+                logging.warning("Código não encontrado!")
     except Exception as e:
-        print(f"Ocorreu um erro em ler_arquivo_txt: {e}")
+        logging.error(f"Ocorreu um erro em ler_arquivo_mea: {e}")
     finally:
         try:
             workbook.close()
@@ -286,9 +299,12 @@ class ArquivoHandler(FileSystemEventHandler):
         time.sleep(1)
         if file_path.endswith('.txt'):
             ler_arquivo_txt_zeiss(file_path)
+            arquivo_nome = os.path.basename(file_path)
+            logging.info(f'Arquivo processado: {arquivo_nome}')
         elif file_path.endswith('.MEA'):
-            ler_arquivo_mea_mistral(file_path)
-        print(f'Arquivo processado: {file_path}')
+            ler_arquivo_mea(file_path)
+            arquivo_nome = os.path.basename(file_path)
+            logging.info(f'Arquivo processado: {arquivo_nome}')
 
     def on_created(self, event):
         if event.is_directory:
@@ -310,4 +326,4 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         observer.stop()
 
-    observer.join()
+    observer.join() 
